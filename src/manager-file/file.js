cabc8e6c96fb7eb23a1da650303f2f4abb209794
alvarengacarlos <carlosho200@hotmail.com>
2022-06-26 13:13:08 -0400

    style: Internalizes variables

--- src/manager-file/file.js
+++ src/manager-file/file.js
@@ -4,58 +4,6 @@ const {CLOUD_PATH} = require("../utils/constants");
 
 const pathToSaveFiles = CLOUD_PATH;
 
-const allowedExtension = [
-	".txt",
-	".html",
-	".md",
-	".json",    
-	".yaml",
-	".xml",
-	""
-];
-
-const charactersNotAllowedInTheFilename = [
-	"/",
-	"\\",
-	"=",
-	"+",
-	"$",
-	"#",
-	"@",
-	"&",    
-	"!",
-	"?",
-	":",
-	"¨",
-	"*",
-	"'",
-	"\"",
-	"|",
-	"<", 
-	">",
-	"^",
-	"~",
-	"`",
-	"`",
-	"{",
-	"}",
-	"[",
-	"]",
-	"(",
-	")",
-	"0",
-	"1",
-	"2",
-	"3",
-	"4",
-	"5",
-	"6",
-	"7",
-	"8",
-	"9",
-	" "
-];
-
 function createFile(filename) {    
 	checkCharacterNotAllowedInTheFilename(filename);
 
@@ -71,13 +19,66 @@ function createFile(filename) {
 }
 
 function checkCharacterNotAllowedInTheFilename(filename) {
+	const charactersNotAllowedInTheFilename = [
+		"/",
+		"\\",
+		"=",
+		"+",
+		"$",
+		"#",
+		"@",
+		"&",    
+		"!",
+		"?",
+		":",
+		"¨",
+		"*",
+		"'",
+		"\"",
+		"|",
+		"<", 
+		">",
+		"^",
+		"~",
+		"`",
+		"`",
+		"{",
+		"}",
+		"[",
+		"]",
+		"(",
+		")",
+		"0",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		" "
+	];
+
 	if (charactersNotAllowedInTheFilename.includes(filename)) {
 		throw new Error(`The filename does not has special characters ${charactersNotAllowedInTheFilename.join(", ")}`);
 	}
 }
 
 function checkFileExtension(filename) {
+	
 	const extension = path.extname(filename);
+	
+	const allowedExtension = [
+		".txt",
+		".html",
+		".md",
+		".json",    
+		".yaml",
+		".xml",
+		""
+	];
 
 	if (!allowedExtension.includes(extension)) {
 		throw new Error("The extension is not allowed");
